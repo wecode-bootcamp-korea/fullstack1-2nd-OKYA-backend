6206381f3d971e6c2df985eb361f041eb298d22e
hxyxneee <wlgus3096@gmail.com>
2021-07-26 18:13:56 +0900

    Modify: 백엔드 초기 세팅 - app.js 수정

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-// import routes from './routes';
 
 const app = express();
 const logger = morgan('dev');
@@ -9,6 +8,5 @@ const logger = morgan('dev');
 app.use(cors());
 app.use(express.json());
 app.use(logger);
-// app.use(routes);
 
 export default app;
