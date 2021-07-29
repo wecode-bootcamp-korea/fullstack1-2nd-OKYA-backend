acde9aa00d7a1db81348541f5017f9694a06b8c0
hxyxneee <wlgus3096@gmail.com>
2021-07-29 18:35:50 +0900

    Add: 파일 구조 추가

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import routes from './routes';
 
 const app = express();
 const logger = morgan('dev');
@@ -8,5 +9,6 @@ const logger = morgan('dev');
 app.use(cors());
 app.use(express.json());
 app.use(logger);
+app.use(routes);
 
 export default app;
